e59e3f5234176755288441a7d6f77e1ffea1f97d
Andy Elvey <andy@obsidian.(none)>
2010-02-14 10:49:01 +1300

    Duh.c can now handle arrow keys and function keys.

--- duh.c
+++ duh.c
@@ -10,12 +10,12 @@
   This code is released to the public domain.                   
 */ 
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <termios.h>
 #include <ctype.h> 
+
 
 void func1(void)
 {  
@@ -42,6 +42,37 @@ void up_arrow(void)
 { 
    printf("Hey, you pressed the up arrow! \n"); 
 }   
+
+void down_arrow(void)
+{ 
+   printf("Hey, you pressed the down arrow! \n"); 
+}
+
+void left_arrow(void)
+{ 
+   printf("Hey, you pressed the left arrow! \n"); 
+}
+
+void right_arrow(void)
+{ 
+   printf("Hey, you pressed the right arrow! \n"); 
+} 
+
+void f2(void)
+{ 
+   printf("Hey, you pressed F2! \n"); 
+} 
+
+void f3(void)
+{ 
+   printf("Hey, you pressed F3! \n"); 
+} 
+
+void f4(void)
+{ 
+   printf("Hey, you pressed F4! \n"); 
+} 
+
 
 
     
@@ -71,12 +102,34 @@ int main(void)
       ctrl_a(); 
   }
       
-   else if (!strcmp(word, "\016") ) { 
+   else if (!strcmp(word, "\x1b\x5b\x41") ) { 
       up_arrow(); 
   }   
       
+   else if (!strcmp(word, "\x1b\x5b\x42") ) { 
+      down_arrow(); 
+  }    
       
-      
+   else if (!strcmp(word, "\x1b\x5b\x43") ) { 
+      right_arrow(); 
+  }   
+   
+   else if (!strcmp(word, "\x1b\x5b\x44") ) { 
+      left_arrow(); 
+  } 
+         
+   else if (!strcmp(word, "\x1b\x4f\x51") ) { 
+      f2(); 
+  } 
+   
+   else if (!strcmp(word, "\x1b\x4f\x52") ) { 
+      f3(); 
+  }
+  
+   else if (!strcmp(word, "\x1b\x4f\x53") ) { 
+      f4(); 
+  }                    
+                                                                                              
    else  { 
    printf("Nope - I do not recognise that phrase.... \n"); 
   }     
